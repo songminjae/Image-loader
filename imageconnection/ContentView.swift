f227e0c352e49ce0a6d96907dc4ae17c10a34b40
songminjae <smj0398@kaist.ac.kr>
2020-01-21 15:40:43 +0900

    fisrt commit

---
+++ imageconnection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  imageconnection
+//
+//  Created by 송민재 on 2020/01/21.
+//  Copyright © 2020 송민재. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
